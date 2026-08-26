ac305c5531ab66f5cb7d338ecfc068b465f48e2b
Nic Smith <nicholas.j.smith-2@student.uts.edu.au>
2026-08-26 14:23:33 +1000

    params
    Signed-off-by: Nic Smith <nicholas.j.smith-2@student.uts.edu.au>

--- launch_multi_robot.sh
+++ launch_multi_robot.sh
@@ -144,4 +144,4 @@ echo "(Ctrl-b d to detach without stopping it.)"
 echo
 echo "Stop everything with: ./stop_multi_robot.sh"
 
-./set_wifi_bandwidth.sh apply --profile degraded --delay-s 100
+./set_wifi_bandwidth.sh apply --profile degraded --delay-s 90
